03ea92cc84bafd3c55033b5a2ce30301bfc897de
Sviatoslav Yakobchuk <s.yakobchuk@viseven.com>
2024-06-23 18:12:47 +0300

    Empty project (first commit)

---
+++ AudioBook/AudioBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBookApp.swift
+//  AudioBook
+//
+//  Created by Sviatoslav Yakobchuk on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AudioBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioBook
+//
+//  Created by Sviatoslav Yakobchuk on 23.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
